ad941578a6b65fe8cd11f79fac60dc0c9e4db28e
Besim <besimsokoli10@gmail.com>
2022-04-16 12:42:00 +0200

    added client and freelance type

--- jobless-no-more-frontend/src/App.tsx
+++ jobless-no-more-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header/Header";
+import ClientSignup from "./pages/ClientSignup";
 import Home from "./pages/Home";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
@@ -36,6 +37,7 @@ function App() {
       <Routes>
         <Route path={"/login"} element={<Login setUser={setUser} />} />
         <Route path={"/signup"} element={<Signup />} />
+        <Route path={"/client-signup"} element={<ClientSignup />} />
         <Route path={"/home"} element={<Home />} />
         <Route index element={<LandingPage user={user} />} />
       </Routes>
---
+++ jobless-no-more-frontend/src/pages/PostJob.tsx
@@ -0,0 +1,4 @@
+function PostJob() {
+  return <div></div>;
+}
+export default PostJob;
